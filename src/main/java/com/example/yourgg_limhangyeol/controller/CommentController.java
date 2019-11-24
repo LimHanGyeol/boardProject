6be41aef8cfb4ddababa36d10c54e34d8e3141f8
LimHanGyeol <dlagksruf19@naver.com>
2019-11-24 17:03:26 +0900

    Revert "댓글 삭제기능 구현"
    This reverts commit 02c99e0999036f534cd65436b3009c75febcd25e.

--- src/main/java/com/example/yourgg_limhangyeol/controller/CommentController.java
+++ src/main/java/com/example/yourgg_limhangyeol/controller/CommentController.java
@@ -2,38 +2,33 @@ package com.example.yourgg_limhangyeol.controller;
 
 import com.example.yourgg_limhangyeol.model.Board;
 import com.example.yourgg_limhangyeol.model.Comment;
-import com.example.yourgg_limhangyeol.model.Result;
 import com.example.yourgg_limhangyeol.repository.BoardRepository;
 import com.example.yourgg_limhangyeol.repository.CommentRepository;
 import com.example.yourgg_limhangyeol.service.comment.CommentCreateService;
-import com.example.yourgg_limhangyeol.service.comment.CommentDeleteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CommentController {
 
     @Autowired
     private CommentCreateService commentCreateService;
-    @Autowired
-    private CommentDeleteService commentDeleteService;
 
-
-    @RequestMapping(method = RequestMethod.POST, value = "/kr/api/board/posts/{no}")
-    public Comment commentCreate(String commentContent) {
+    @PostMapping("/kr/api/board/posts/{no}")
+    public Comment test(String commentContent) {
 
         return commentCreateService.commentCreate(commentContent);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/kr/api/board/posts/{no}/comment/{commentNo}")
-    public Result commentDelete(@PathVariable Long commentNo) {
-        System.out.println("commentNO : "+commentNo);
-        Comment comment = commentDeleteService.getComment(commentNo);
-        commentDeleteService.commentDelete(comment);
-        return Result.ok();
-    }
-
+//    @GetMapping("/kr/api/board/post/{no}")
+//    public Board test2(@PathVariable Long no) {
+//
+//        return boardRepository.findById(no).get();
+//    }
 
 
 }
--- src/main/java/com/example/yourgg_limhangyeol/service/comment/CommentDeleteService.java
+++ src/main/java/com/example/yourgg_limhangyeol/service/comment/CommentDeleteService.java
@@ -1,21 +1,4 @@
 package com.example.yourgg_limhangyeol.service.comment;
 
-import com.example.yourgg_limhangyeol.model.Comment;
-import com.example.yourgg_limhangyeol.repository.CommentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
 public class CommentDeleteService {
-
-    @Autowired
-    private CommentRepository commentRepository;
-
-    public Comment getComment(Long commentNo) {
-        return commentRepository.findById(commentNo).get();
-    }
-
-    public void commentDelete(Comment comment) {
-        commentRepository.delete(comment);
-    }
 }
